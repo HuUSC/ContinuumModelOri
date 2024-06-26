402a806711e8bed09881622bf9cf0c29e53fbca9
Frederic Marazzato <marazzato@arizona.edu>
2024-06-26 15:22:11 -0700

    Saddle computation works!

--- 1d_sol/bending.py
+++ 1d_sol/bending.py
@@ -46,9 +46,8 @@ final.write(theta)
 
 #Interpolate results from the ode computation
 W = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
+PETSc.Sys.Print('Nb vec dof: %i' % W.dim())
 omega_u = Function(W, name='omega_u')
-#print(omega_u.vector()[:,1])
-#sys.exit()
 omega_u.vector()[:,1] = np.interp(coords, t, sol_omega_u[1,:])
 omega_v = Function(W, name='omega_v')
 omega_v.vector()[:,0] = np.interp(coords, t, sol_omega_v[0,:])
@@ -61,63 +60,53 @@ final = VTKFile('omega_v.pvd')
 final.write(omega_v)
 
 #Computing the rotation
-W = TensorFunctionSpace(mesh, 'CG', 1, shape=(3,3))
-PETSc.Sys.Print('Nb tensor dof: %i' % W.dim())
+Z = TensorFunctionSpace(mesh, 'CG', 1, shape=(3,3))
+PETSc.Sys.Print('Nb tensor dof: %i' % Z.dim())
 
 #Dirichlet BC
-bcs = [DirichletBC(W, Identity(3), 1)]
+bcs = [DirichletBC(Z, Identity(3), 1)]
 
 #Auxiliary fields
 Omega_u = as_tensor(((0, -omega_u[2], omega_u[1]), (omega_u[2], 0, -omega_u[0]), (-omega_u[1], omega_u[0], 0)))
 Omega_v = as_tensor(((0, -omega_v[2], omega_v[1]), (omega_v[2], 0, -omega_v[0]), (-omega_v[1], omega_v[0], 0)))
 
 #Bilinear form
-S = TrialFunction(W)
-T = TestFunction(W)
+S = TrialFunction(Z)
+T = TestFunction(Z)
 a = inner(S.dx(1) - dot(S, Omega_v), T.dx(1) - dot(T, Omega_v)) * dx + inner(S.dx(0) - dot(S, Omega_u), T.dx(0) - dot(T, Omega_u)) * dx
 l = Constant(0) * T[0,0] * dx(mesh)
 
 #Linear solve
-R = Function(W, name='rotation')
-solve(a == l, R, bcs=bcs)
+Reff = Function(Z, name='rotation')
+solve(a == l, Reff, bcs=bcs)
 
 #plotting the result
 final = VTKFile('rot.pvd')
-final.write(R)
-sys.exit()
+final.write(Reff)
 
 #Recomputing the effective deformation
-X = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
-PETSc.Sys.Print('Nb vec dof: %i' % X.dim())
 
 # the coefficient functions
-u = sqrt(3) * cos(0.5*(theta+theta_0)) * acos(0.5*theta_0) * Constant((1, 0))
-v = 2 * sqrt ( (5-3*cos(theta_0)) / (5-3*cos(theta+theta_0))) * Constant((0, 1))
-
-#Writing the matrix A_eff
-Rot = Constant(((0,-1), (1,0)))
-A11 = -dot(u,diff(v,theta))
-A12 = dot(u,diff(u,theta))
-A21 = -dot(v,diff(v,theta))
-A22 = dot(v,diff(u,theta))
-A = as_tensor(((A11, A12), (A21, A22))) / dot(u, dot(Rot, v))
+lv = sqrt(3) * cos(0.5*(theta+phi))
+lu = 2 * sqrt ( 2 / (5-3*cos(theta+phi)))
 
 
 #Bilinear form
-u = TrialFunction(X)
-v = TestFunction(X)
+u = TrialFunction(W)
+v = TestFunction(W)
 a = inner(grad(u), grad(v)) * dx
-A_aux = as_tensor(((A[0,0], A[0,1]), (A[1,0], A[1,1]), (0, 0)))
-l = inner(dot(R, A_aux), grad(v)) * dx
+Aeff = as_tensor(((lu, 0), (0, lv), (0, 0)))
+l = inner(dot(Reff, Aeff), grad(v)) * dx
 
 #Linear solve
-yeff = Function(X, name='y_eff')
+yeff = Function(W, name='yeff')
 nullspace = VectorSpaceBasis(constant=True)
 solve(a == l, yeff, nullspace=nullspace)
 
 #plotting the result
-aux = interpolate(yeff-as_vector((x[0], x[1], 0)), X)
-final = File('yeff.pvd')
+aux = Function(W, name='yeff 3d')
+aux.interpolate(yeff-as_vector((x[0], x[1], 0)))
+final = VTKFile('yeff.pvd')
 final.write(aux)
 #final.write(yeff)
 
