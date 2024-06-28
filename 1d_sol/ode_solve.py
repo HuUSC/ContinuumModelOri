8a74c46838abf42725a530dbff6b85a7cce42fbe
Frederic Marazzato <marazzato@arizona.edu>
2024-06-28 15:25:33 -0700

    Corrected the 1d solution a bit. Check if now solve works.

--- 1d_sol/ode_solve.py
+++ 1d_sol/ode_solve.py
@@ -3,27 +3,26 @@ import numpy as np
 # ## ODE solve -- actuation field(\theta)
 def actuation(vec, x, phi, c_tau, c_kappa):
     theta, d_theta = vec
-    lu = 2.0 * np.sqrt( 2/(5 - 3 * np.cos( theta+phi )) )
-    lv = np.sqrt(3.0) * np.cos( (theta+phi)/2.0 )
-    lpu = - 3 * np.sqrt(2) * np.sin(theta+phi)/(5 - 3 * np.cos( theta+phi ))**(3/2)
-    lpv = - np.sqrt(3) * np.sin( (theta+phi)/2 ) / 2
+    lv = 2.0 * np.sqrt( 2/(5 - 3 * np.cos( theta+phi )) )
+    lu = np.sqrt(3.0) * np.cos( (theta+phi)/2.0 )
+    lpv = - 3 * np.sqrt(2) * np.sin(theta+phi)/(5 - 3 * np.cos( theta+phi ))**(3/2)
+    lpu = - np.sqrt(3) * np.sin( (theta+phi)/2 ) / 2
     d = ( 3 * np.cos( 3*(theta+phi)/2 ) - 5 * np.cos( (theta+phi)/2 ) ) / 8 / np.sqrt( 10/3 - 2 * np.cos( theta+phi ) )
-    equ = [d_theta, - d * lu/lpv * d_theta**2 + c_tau**2 * lu**2/lpv/lv**3 + c_kappa**2 * lpu/lu/lpv**2]
+    equ = [d_theta, - d * lv/lpu * d_theta**2 + c_tau**2 * lv**2/lpu/lu**3 + c_kappa**2 * lpv/lv/lpu**2]
     return equ
 
 # solutions to bend and twist fields
 def bendtwist(vec, phi, c_tau, c_kappa):
     theta, d_theta = vec[:, 0], vec[:, 1]
-    # lu0 = 2.0 * np.sqrt(2 / (5 - 3 * np.cos(phi)))
-    lu = 2.0 * np.sqrt(2 / (5 - 3 * np.cos(theta + phi)))
-    lv = np.sqrt(3) * np.cos((theta + phi) / 2)
-    lpu = - 3 * np.sqrt(2) * np.sin(theta + phi) / (5 - 3 * np.cos(theta + phi)) ** (3 / 2)
-    lpv = - np.sqrt(3) * np.sin((theta + phi) / 2) / 2
-    kappa = c_kappa / lv/lu**2/lpv
-    tau = c_tau / lv**2
+    lv = 2.0 * np.sqrt(2 / (5 - 3 * np.cos(theta + phi)))
+    lu = np.sqrt(3) * np.cos((theta + phi) / 2)
+    lpv = - 3 * np.sqrt(2) * np.sin(theta + phi) / (5 - 3 * np.cos(theta + phi)) ** (3 / 2)
+    lpu = - np.sqrt(3) * np.sin((theta + phi) / 2) / 2
+    kappa = c_kappa / lu/lv**2/lpu
+    tau = c_tau / lu**2
     omegau_1 = tau * lu
     omegau_2 = kappa * lu * lpu * lv
-    omegau_3 = 0.0 * lu
+    omegau_3 = -d_theta * lpu / lv #0.0 * lu #correct?
     omegav_1 = kappa * lpv * lv * lu
     omegav_2 = - tau * lv
     omegav_3 = d_theta * lpv/lu
