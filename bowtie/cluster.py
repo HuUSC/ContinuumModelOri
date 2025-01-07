215b8a491afe5deda90cc23f8e0c3a78624d191d
Frederic Marazzato <marazzato@arizona.edu>
2025-01-07 11:31:10 -0700

    Updated to have the correct BC. Imposing only the x direction not the y direction too.

--- bowtie/cluster.py
+++ bowtie/cluster.py
@@ -7,6 +7,10 @@ import numpy as np
 # Create mesh
 mesh = Mesh('mesh.msh', name='mesh')
 
+#Save mesh to file
+with CheckpointFile("res.h5", 'w') as afile:
+        afile.save_mesh(mesh)
+
 # Define function spaces
 V = VectorFunctionSpace(mesh, "CG", 2)
 W = FunctionSpace(mesh, "CG", 1)
@@ -19,10 +23,6 @@ x = SpatialCoordinate(mesh)
 #BC
 bnd = as_vector(((1-2*val)*x[0] + val, x[1]))
 
-##Initial guess in displacement
-#sol_ig = Function(V, name='IG')
-#sol_ig.interpolate(as_vector(((1-2*val)*x[0] + val, x[1])))
-
 #Interior penalty
 alpha = Constant(1e-1) #1e-1 #penalty parameter
 h = CellDiameter(mesh) # cell diameter
@@ -30,7 +30,7 @@ h_avg = avg(h)  # average size of cells sharing a facet
 n = FacetNormal(mesh) # outward-facing normal vector
 
 # elastic parameters
-c_1 = 1 #metric constraint
+c_1 = 5 #metric constraint
 d_1 = .1
 d_2, d_3 = 1e-2, 1e-2
 
@@ -44,17 +44,14 @@ sol = Function(Z, name='sol')
 y, theta = split(sol)
 
 #Interpolate initial guess
-#sol.sub(0).interpolate(bnd)
+sol.sub(0).interpolate(bnd)
 
-#Load the initial guess from a file
-with CheckpointFile("res.h5", 'r') as afile:
-    meshR = afile.load_mesh("mesh")
-    sol_old = afile.load_function(meshR, "sol", val)
-    sol.interpolate(sol_old)
-#sys.exit()
-
-#Define the boundary conditions
-bcs = [DirichletBC(Z.sub(0), bnd, 1), DirichletBC(Z.sub(0), bnd, 2)]
+##Load the initial guess from a file
+#with CheckpointFile("res.h5", 'r') as afile:
+#    meshR = afile.load_mesh("mesh")
+#    sol_old = afile.load_function(meshR, "sol", val)
+#    sol.interpolate(sol_old)
+##sys.exit()
 
 # basis vectors & reference/deformed Bravais lattice vectors & metric tensor
 phi = pi/6
@@ -89,12 +86,6 @@ en_pen = inner( dot(avg(G), n('+')), jump(grad(y))) * dS # consistency and symme
 a -= derivative(en_pen, y, w)
 a += alpha / h_avg * inner( jump( grad(y), n ), jump( grad(w), n ) ) * dS #pen term
 
-##Gradient BC
-#a += alpha / h * inner(dot(dot(grad(y), n), n), dot(dot(grad(w), n), n)) * (ds(1) + ds(2)) #lhs pen
-#en_pen = inner(dot(dot(G, n), n), dot(grad(y), n)) * (ds(1) + ds(2)) # consistency and symmetry energy term
-#a -= derivative(en_pen, y, w)
-#a -= alpha / h * inner(dot(dot(grad(bnd), n), n), dot(dot(grad(w), n), n)) * (ds(1) + ds(2))  #rhs pen
-#a -= replace(derivative(en_pen, y, w), {y:bnd}) #rhs consistency and symmetry term
 
 #Solve parameters
 #parameters={"snes_monitor": None, "ksp_type": "preonly", "mat_type": "aij", "pc_type": "lu", "pc_factor_mat_solver_type": "mumps"}
@@ -102,40 +93,40 @@ parameters = {'snes_monitor': None, 'snes_max_it': 25, 'quadrature_degree': '4',
 v_basis = VectorSpaceBasis(constant=True)
 nullspace = MixedVectorSpaceBasis(Z, [v_basis, Z.sub(1)])
 
-disp_max = .45
+disp_max = .42
 d_disp = 1e-3
+idx = 0
 while val < disp_max:
     #Increase disp
     val += d_disp
+    idx += 1
     PETSc.Sys.Print('Disp: %.3f' % val)
 
     #Define the boundary conditions
-    bnd = as_vector(((1-2*val)*x[0] + val, x[1]))
-    bcs = [DirichletBC(Z.sub(0), bnd, 1), DirichletBC(Z.sub(0), bnd, 2)]
+    #bnd = as_vector(((1-2*val)*x[0] + val, x[1]))
+    bnd = (1-2*val)*x[0] + val
+    bcs = [DirichletBC(Z.sub(0).sub(0), bnd, 1), DirichletBC(Z.sub(0).sub(0), bnd, 2)]
 
     #Solve
     solve(a == 0, sol, bcs=bcs, nullspace=nullspace, solver_parameters=parameters)
 
     #Save results
-    with CheckpointFile("res.h5", 'w') as afile:
-        afile.save_mesh(mesh)
-        afile.save_function(sol, val)
+    with CheckpointFile("res.h5", 'a') as afile:
+        afile.save_function(sol, idx=idx)
 
     #Computing reaction forces
     v_reac = Function(Z)
     bc_l = DirichletBC(V.sub(0), Constant(1), 1)
     bc_l.apply(v_reac.sub(0))
-    #res_l = assemble(action(a, v_reac))
-    #print('Reaction on the left: %.3e' % assemble(res))
-    #v_reac.sub(0).interpolate(Constant((0, 0)))
-    bc_r = DirichletBC(V.sub(0), Constant(-1), 2)
-    bc_r.apply(v_reac.sub(0))
-    res_r = assemble(action(a, v_reac))
-    res = assemble(action(a, v_reac))
-    #print('Reaction on the right: %.3e' % assemble(res))
-    #PETSc.Sys.Print('Disp: %.3e' % val)
-    #PETSc.Sys.Print('Total force: %.3e' % (abs(res_l)+abs(res_r)))
+    res_l = assemble(action(a, v_reac))
+    #bc_r = DirichletBC(V.sub(0), Constant(-1), 2)
+    #bc_r.apply(v_reac.sub(0))
+    #res_r = assemble(action(a, v_reac))
+    #res = assemble(action(a, v_reac))
+    ##print('Reaction on the right: %.3e' % assemble(res))
+    ##PETSc.Sys.Print('Disp: %.3e' % val)
+    ##PETSc.Sys.Print('Total force: %.3e' % (abs(res_l)+abs(res_r)))
 
     #Save forces
     with open('force.txt', 'a') as f:
-        np.savetxt(f, np.array([val, res])[None], delimiter=',', fmt='%.3e')
+        np.savetxt(f, np.array([2*val, res_l])[None], delimiter=',', fmt='%.3e')
