1fb72db364792f6f0d92c7e033347b0f1c86c1f1
Hu Xu <154289216+HuUSC@users.noreply.github.com>
2025-01-27 15:08:20 -0800

    Corrected u_t_p and v_t_p

--- bowtie_Miura/bowtie_Miura.py
+++ bowtie_Miura/bowtie_Miura.py
@@ -60,11 +60,14 @@ bcs = [DirichletBC(Z.sub(0), bnd, 1), DirichletBC(Z.sub(0), bnd, 2)] #bowtie
 
 
 # basis vectors & reference/deformed Bravais lattice vectors & metric tensor
-u_ts = sqrt(3) * cos( (theta+phi)/2 )
-v_ts = 2 * sqrt( 2/ ( 5-3 * cos(theta+phi) ) )
+theta = variable(theta)
+u_ts = sqrt(3.0) * cos( (theta+phi)/2 )
+v_ts = 2 * sqrt( 2.0/ ( 5-3 * cos(theta+phi) ) )
 A_t = as_matrix( [ [ u_ts/ u_s, 0], [0, v_ts/v_s] ] )
-u_t_p = diff(u_ts, variable(theta)) 
-v_t_p = diff(v_ts, variable(theta)) 
+u_t_p = diff(u_ts, theta)
+v_t_p = diff(v_ts, theta)
+# u_t_p = - sqrt(3.0)/2 * sin( (theta+phi)/2 )
+# v_t_p = - 3 * sqrt(2) * sin(theta+phi) / ( ( 5-3 * cos(theta+phi) )**(3/2) )
 
 #Preparation for variational form
 H = variable(grad(grad(y)))
